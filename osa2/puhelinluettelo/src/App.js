c942d56c7d88f50d06634f5d87680747e0d59602
Salla <salla.tero@gmail.com>
2019-03-21 13:23:00 +0200

    Osa 2 tehty valmiiksi

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -4,12 +4,15 @@ import Numerot from './components/Numerot'
 import Uusi from './components/Uusi'
 import Rajaa from './components/Rajaa'
 import personService from './services/persons'
+import Kayttajaviestit from './components/Kayttajaviestit'
 
 const App = () => {
     const [persons, setPersons] = useState([])
     const [newName, setNewName] = useState('')
     const [newNro, setNewNro] = useState('')
     const [search, setSearch] = useState('')
+    const [message, setMessage] = useState('')
+    const [err, setErr] = useState(false)
 
     //Haetaan data palvelimelta
     useEffect(() => {
@@ -35,9 +38,9 @@ const App = () => {
     const handleSubmit = (event) => {
         event.preventDefault()
         
-        //Tarkistetaan onko nimi jo taulukossa, jos ei, lisätään
+        //Tarkistetaan onko nimi jo taulukossa
         let is = persons.find(e => 0 === newName.localeCompare(e.name, undefined, {sensitivity: 'base'}))
-        if (is === undefined){
+        if (is === undefined){ //Jos ei, tallennetaan suoraan
             const obj = {
                 name: newName,
                 number: newNro
@@ -45,12 +48,47 @@ const App = () => {
             personService
             .create(obj)
             .then(returnedPers => {
-                console.log('uusi obj lisätty: ', obj)
                 setPersons(persons.concat(returnedPers))
+                setMessage(`Lisättiin ${returnedPers.name}`)
+                setErr(false)
+                setTimeout(() => {
+                    setMessage('')
+                }, 5000)
             })
           }else{
-            console.log('NIMI ON JO')
-            alert(`${newName} on jo luettelossa`)
+            console.log('Nimi on jo.')
+            //Käyttäjän täytyy vahvistaa
+            const answer = window.confirm(`${newName} on jo luettelossa. Korvataanko vanha numero uudella?`)
+            if (answer) { //Korvataan
+                //Haetaan person jolla sama nimi
+                const pers = persons.find(p => p.name === newName)
+                //Luodaan uusi olio, joka on kopio mutta muutetaan numero
+                const modifiedPers = {...pers, number: newNro}
+                personService
+                .update(pers.id, modifiedPers)
+                .then(returnedPers => {
+                    setPersons(persons.map(p => p.id !== pers.id ? p : returnedPers))
+                    setMessage(`Käyttäjän ${newName} numero päivitettiin`)
+                    setErr(false)
+                    setTimeout(() => {
+                        setMessage('')
+                    }, 5000)
+                })
+                .catch(error => {
+                    setMessage(`Käyttäjä ${newName} oli jo poistettu.`)
+                    setErr(true)
+                    setTimeout(() => {
+                        setMessage('')
+                    }, 5000)
+                    //Renderöinti??
+                    personService
+                    .getAll()
+                    .then(allPersons => {
+                        console.log('LADATAAN PALVELIMELTA')
+                        setPersons(allPersons)
+                    }) 
+                })
+            }
           }
           reset()
     }
@@ -73,6 +111,18 @@ const App = () => {
             .then(resp => {
                 console.log(resp)
                 setPersons(persons.filter(p => p.id !== id))
+                setMessage(`Poistettiin ${name}`)
+                setErr(false)
+                setTimeout(() => {
+                    setMessage('')
+                }, 5000)
+            })
+            .catch(error => {
+                setMessage(`Käyttäjä ${newName} oli jo poistettu.`)
+                setErr(true)
+                setTimeout(() => {
+                    setMessage('')
+                }, 5000)
             })
         } else {
             console.log('peruutettiin');
@@ -82,6 +132,7 @@ const App = () => {
     return (
         <div>
             <h1>Puhelinluettelo</h1>
+            <Kayttajaviestit message={message} err={err}/>
             <Rajaa search={search} handler={handleChangeSearch}/>
             <h2>Lisää uusi</h2>
             <Uusi handleSubmit={handleSubmit} newName={newName} newNro={newNro} changeName={handleChangeName} changeNro={handleChangeNro}/>
