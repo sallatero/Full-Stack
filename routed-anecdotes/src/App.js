ed1ded44990c829722235dae8d1baa2411b3fbb7
Salla <salla.tero@gmail.com>
2019-05-01 12:09:18 +0300

    7.2: Yksittäisen anekdootin näkymä lisätty

--- routed-anecdotes/src/App.js
+++ routed-anecdotes/src/App.js
@@ -21,8 +21,20 @@ const AnecdoteList = ({ anecdotes }) => (
   <div>
     <h2>Anecdotes</h2>
     <ul>
-      {anecdotes.map(anecdote => <li key={anecdote.id} >{anecdote.content}</li>)}
+      {anecdotes.map(anecdote => 
+        <li key={anecdote.id} >
+          <Link to={`/anecdotes/${anecdote.id}`}>{anecdote.content}</Link>
+        </li>
+      )}
     </ul>
+  </div>
+)
+
+const Anecdote = ({ anecdote }) => (
+  <div>
+    <h2>{anecdote.content} by {anecdote.author}</h2>
+    <p>has {anecdote.votes} votes</p>
+    <p>for more info see <a href={anecdote.info}>{anecdote.info}</a></p>
   </div>
 )
 
@@ -134,6 +146,8 @@ const App = () => {
           <Menu />     
             <Route exact path='/' render={() => 
               <AnecdoteList anecdotes={anecdotes}/>}/>
+            <Route path='/anecdotes/:id' render={({ match }) => 
+              <Anecdote anecdote={anecdoteById(match.params.id)}/>}/>
             <Route path='/create' render={() => 
               <CreateNew addNew={addNew}/>}/>
             <Route path='/about' render={() => 
