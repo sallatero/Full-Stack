951a6c2450ede0231e7170a536a798f42e180abb
Salla <salla.tero@gmail.com>
2019-05-13 11:39:54 +0300

    7.16: Webpack asennettu ja konfiguroitu webpack-bloglist-projektiin

---
+++ webpack-bloglist/src/App.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const App = () => (
+  <div className='container'>hello webpack</div>
+)
+
+export default App
---
+++ webpack-bloglist/webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path')
+
+const config = {
+  entry: ['@babel/polyfill', './src/index.js'],
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'main.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['@babel/preset-env', '@babel/preset-react']
+        }
+      },
+      {
+        test: /\.css$/,
+        loaders: ['style-loader', 'css-loader']
+      }
+    ]
+  }
+}
+module.exports = config
