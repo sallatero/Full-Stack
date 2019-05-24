0f4a4c8ed6b53a52a07f58a35d4b5926f51c363b
Salla <salla.tero@gmail.com>
2019-05-24 12:43:38 +0300

    8.16 me-query toimii

--- graphql-kirjasto/index.js
+++ graphql-kirjasto/index.js
@@ -92,6 +92,9 @@ const resolvers = {
     allAuthors: async () => {
       console.log('allAuthors query')
       return Author.find({})
+    },
+    me: (root, args, context) => {
+      return context.currentUser
     }
   },
   Author: {
@@ -185,6 +188,16 @@ const resolvers = {
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: async ({ req }) => {
+    const auth = req ? req.headers.authorization : null
+    if (auth && auth.toLowerCase().startsWith('bearer ')) {
+      const decodedToken = jwt.verify(
+        auth.substring(7), JWT_SECRET
+      )
+      const currentUser = await User.findById(decodedToken.id)
+      return { currentUser }
+    }
+  }
 })
 
 server.listen().then(({ url }) => {
